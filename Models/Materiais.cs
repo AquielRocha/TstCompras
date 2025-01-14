06cf4f618b2a545e53341298c763bb6fc8d51e2d
aquiel <restefino1@gmail.com>
2025-01-14 08:51:32 -0300

    fix: coments

--- Models/Materiais.cs
+++ Models/Materiais.cs
@@ -6,7 +6,7 @@ namespace TstCompras.Models
     {
         public int id { get; set; }
         public int codigo_item { get; set; }
-        public string nome_item { get; set; }
+        public string nome_item { get; set; } // equivale ao nome do pdm
         public string descricao_item { get; set; }
         public int codigo_grupo { get; set; }
         public string nome_grupo { get; set; }
